9522e0ff313441da03c7afb225a85145d04ca2bd
Yan Cervantes <acervantes@ia.com.mx>
2025-06-04 03:38:58 +0200

    **Feature:** Initial Commit

---
+++ eCommerse-iOS/eCommerse-iOS/eCommerse_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  eCommerse_iOSApp.swift
+//  eCommerse-iOS
+//
+//  Created by Yan Cervantes on 04/06/25.
+//
+
+import SwiftUI
+
+@main
+struct eCommerse_iOSApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
